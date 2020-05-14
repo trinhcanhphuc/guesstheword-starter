28765b5f2a24744b463e4fb99c5f41d74a23a917
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-05-15 00:26:00 +0700

    Add the Play Again button

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -8,10 +8,21 @@ import androidx.lifecycle.ViewModel
 class ScoreViewModel(finalScore: Int) : ViewModel() {
     // the final score
     private val _score = MutableLiveData<Int>()
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+
     val score: LiveData<Int>
         get() = _score
 
     init {
         _score.value = finalScore
+    }
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
     }
 }
