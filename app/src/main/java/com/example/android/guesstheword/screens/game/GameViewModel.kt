45dcdfd2552c7797a86a7fd60e45023588433166
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-05-15 23:19:16 +0700

    Add word hint

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -43,6 +43,12 @@ class GameViewModel : ViewModel() {
         DateUtils.formatElapsedTime(time)
     }
 
+    val wordHint = Transformations.map(word) { word ->
+        val randomPosition = (1..word.length).random()
+        "Current word has " + word.length + " letters" +
+                "\nThe letter at position " + randomPosition + " is " +
+                word.get(randomPosition - 1).toUpperCase()
+    }
 
     /**
      * Resets the list of words and randomizes the order
